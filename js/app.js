fa259c90c2dc7f9ef475bbe7969e554c17efd5a1
Tom Taylor <tomgtaylor@yahoo.com>
2022-07-28 14:08:43 -0700

    Was able to add total sales per location in an <li>

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 //------------------------------------------ Global variables / functions
 
     // creates an array of hours.
-let hours = [`6am`, `7am`, `8am`, `9am`, `10am`, `11am`, `12pm`, `1pm`, `2pm`, `3pm`, `4pm`, `5pm`, `6pm`, `7pm`];
+let hours = [`6:00 am`, `7:00 am`, `8:00 am`, `9:00 am`, `10:00 am`, `11:00 am`, `12:00 pm`, `1:00 pm`, `2:00 pm`, `3:00 pm`, `4:00 pm`, `5:00 pm`, `6:00 pm`, `7:00 pm`];
 
 //--------------------
 function randomNumberGen(minimum, maximum) {
@@ -18,13 +18,16 @@ let seattle = {
     minimum: 23,
     maximum: 65,
     avgCookiesPerHour: 6.3,
+    totalSales: [],
 };
 
 let tokyo = {
     name: 'Tokyo',
     minimum: 3,
     maximum: 24,
-    avgCookiesPerHour: 1.2
+    avgCookiesPerHour: 1.2,
+    totalSales: [],
+    
 };
 
 let dubai = {
@@ -32,6 +35,7 @@ let dubai = {
     minimum: 11,
     maximum: 38,
     avgCookiesPerHour: 3.7,
+    totalSales: [],
 };
 
 let paris = {
@@ -39,6 +43,7 @@ let paris = {
     minimum: 20,
     maximum: 38,
     avgCookiesPerHour: 2.3,
+    totalSales: [],
 };
 
 let lima = {
@@ -46,29 +51,48 @@ let lima = {
     minimum: 2,
     maximum: 16,
     avgCookiesPerHour: 4.6,
+    totalSales: [],
 };
 
 let locations = [seattle, tokyo, dubai, paris, lima];
-// let totalCookies = 0;
 
 //-----------------------------------------
-    // for loop iterating through array "locations"
+// for loop iterates through array "locations" 
 for(let i = 0; i < locations.length; i++){
     let location = locations[i];
     let list = document.getElementById(location.name.toLowerCase());
 
-        //nested for loop going through hours from left to right in array "hours"
+        // total starts at 0, increments per hour iteration, and restarts with every new location index.
+    let total = 0;
+       
+    //nested for loop iterates through array "hours"
     for(let j = 0; j < hours.length; j++){
-
+        
         let hour = hours[j];
-
+        
         let randomNum = randomNumberGen(location.minimum, location.maximum);
         let avgCookies = Math.round(location.avgCookiesPerHour * randomNum);
-        console.log(hour, randomNum, avgCookies);
 
+            // this targets the current index in location array, targets totalSales array, and pushes avgCookies amount into it.
+        locations[i].totalSales.push(avgCookies);
+
+            // += is the same as total = total + avgCookies. Total here addes all avg cookies iteration.
+        total += avgCookies;
+        // console.log(hour, randomNum, avgCookies);
+        
         let li = document.createElement('li');
         li.innerText = `${hour}: ${avgCookies} cookies`;
         list.appendChild(li);
+       
     }
+    
+    // After all iterations of hours array, code runs below, which adds another <li> 
+
+    let li = document.createElement('li');
+    li.innerText = `Total Cookies Sold in ${location.name}: ${total} Cookies`;
+    list.appendChild(li);
+    // console.log(total);
 }
+
+
 
